e61e7a23a6041962c2108a81e1cc4aadeeff82fe
David-223 <david1kopecky@gmx.de>
2026-04-05 21:16:59 +0200

    Update hands-data.js

--- public/hands-data.js
+++ public/hands-data.js
@@ -38,7 +38,7 @@ const HANDS = [
       { v: 'A♦', r: true  }, { v: 'A♣', r: false }, { v: 'K♠', r: false }
     ],
     desc: 'Vier Karten desselben Wertes.',
-    def:  'Verteile 6 Schlucke frei. +1 Domain-Münze 🪙',
+    def:  'Verteile 6 Schlücke. +1 Domain-Münze 🪙',
   },
   {
     id:    'full_house',
